9ec8410cce7aaf64733a8965045eeb7cae34700e
Thomas Kadauke <tkadauke@imedo.de>
2013-03-22 00:18:41 +0100

    Quadratic equation solver

---
+++ include/math/Quadric.h
@@ -0,0 +1,48 @@
+#ifndef QUADRIC_H
+#define QUADRIC_H
+
+#include <limits>
+#include <cmath>
+
+template<class T>
+class Quadric {
+public:
+  typedef T Coefficient;
+  typedef T Result[3];
+  
+  Quadric(T a, T b, T c)
+    : m_a(a), m_b(b), m_c(c)
+  {
+    m_result[0] = std::numeric_limits<T>::quiet_NaN();
+    m_result[1] = std::numeric_limits<T>::quiet_NaN();
+    m_result[2] = std::numeric_limits<T>::quiet_NaN();
+  }
+  
+  int solve();
+  inline const Result& result() const { return m_result; }
+
+private:
+  T m_a, m_b, m_c;
+  Result m_result;
+};
+
+template<class T>
+int Quadric<T>::solve() {
+  static const T epsilon = std::numeric_limits<double>::epsilon();
+  
+  double determinant = m_b * m_b - 4 * m_a * m_c;
+
+  if (-epsilon < determinant && determinant < epsilon) {
+    m_result[0] = - m_b / (2 * m_a);
+    return 1;
+  } else if (determinant > 0) {
+    double determinantRoot = std::sqrt(determinant);
+
+    m_result[0] = (determinantRoot - m_b) / (2 * m_a);
+    m_result[1] = (-determinantRoot - m_b) / (2 * m_a);
+    return 2;
+  } else
+    return 0;
+}
+
+#endif
---
+++ test/unit/math/QuadricTest.cpp
@@ -0,0 +1,37 @@
+#include "gtest.h"
+#include "math/Quadric.h"
+
+namespace QuadricTest {
+  template<class T>
+  class QuadricTest : public ::testing::Test {
+  };
+
+  typedef ::testing::Types<Quadric<float>, Quadric<double> > QuadricTypes;
+
+  TYPED_TEST_CASE(QuadricTest, QuadricTypes);
+
+  TYPED_TEST(QuadricTest, ShouldInitializeResult) {
+    TypeParam quadric(0, 0, 0);
+    ASSERT_TRUE(std::isnan(quadric.result()[0]));
+    ASSERT_TRUE(std::isnan(quadric.result()[1]));
+    ASSERT_TRUE(std::isnan(quadric.result()[2]));
+  }
+
+  TYPED_TEST(QuadricTest, ShouldSolveQuadricWithNoResult) {
+    TypeParam quadric(1, 0, 1);
+    ASSERT_EQ(0, quadric.solve());
+  }
+
+  TYPED_TEST(QuadricTest, ShouldSolveQuadricWithOneResult) {
+    TypeParam quadric(1, 0, 0);
+    ASSERT_EQ(1, quadric.solve());
+    ASSERT_EQ(0, quadric.result()[0]);
+  }
+
+  TYPED_TEST(QuadricTest, ShouldSolveQuadricWithTwoResults) {
+    TypeParam quadric(1, 0, -1);
+    ASSERT_EQ(2, quadric.solve());
+    ASSERT_EQ(1, quadric.result()[0]);
+    ASSERT_EQ(-1, quadric.result()[1]);
+  }
+}
