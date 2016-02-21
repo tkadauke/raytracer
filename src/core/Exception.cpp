e77459b227aecc5323f6ac9640ed83de3ba70a1a
Thomas Kadauke <tkadauke@imedo.de>
2016-02-20 23:35:02 -0800

    Small crash handler to debug out-of-UI or very early crashes

--- src/core/Exception.cpp
+++ src/core/Exception.cpp
@@ -26,3 +26,26 @@ void Exception::printBacktrace() {
     cout << i << endl;
   }
 }
+
+class Trap {
+  typedef void (*Handler)(int);
+public:
+  Trap(int sig, Handler handler)
+  {
+    signal(sig, handler);
+  }
+};
+
+static void crashHandler(int sig) {
+  void *array[200];
+  size_t size;
+
+  size = backtrace(array, 200);
+
+  cerr << "Error: signal " << sig << ":" << endl;
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+// Uncomment the following line to get a backtrace on SIGSEGV in the CLI
+// static Trap sigsegv(SIGSEGV, crashHandler);
