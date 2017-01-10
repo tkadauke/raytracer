bfe232f59f32b9e994e0b0e7be8270e1a297f22d
Thomas Kadauke <tkadauke@Thomass-MacBook-Pro-3.local>
2017-01-10 01:48:50 -0800

    Add a ton of units to Angle class

--- include/core/math/Angle.h
+++ include/core/math/Angle.h
@@ -8,12 +8,22 @@
 /**
   * This class represents an angle. It is a simple wrapper around a float or
   * double (type parameter T), but it makes angular calculations explicit and
-  * type safe. Angles can be constructed and converted to one of several units:
+  * type safe. Angles can be constructed and converted to one of several units
+  * (See https://en.wikipedia.org/wiki/Angle#Units for more information):
   * 
   * * Radians
+  * * Grads
   * * Degrees
+  * * Arc minutes
+  * * Arc seconds
+  * * Hexacontades
+  * * Binary degrees
   * * Turns
+  * * Quadrants
+  * * Sextants
   * * o'Clock
+  * * Hours
+  * * Compass points
   * 
   * Internally, the angle is stored in radians, which makes calculations easy
   * for all the functions that use radians.
@@ -53,6 +63,39 @@ public:
   }
   
   /**
+    * Constructs an angle from @p arcMinutes. An arc minute is
+    * \f$\frac{1}{60}\f$ of a degree.
+    */
+  inline static Angle<T> fromArcMinutes(const T& arcMinutes) {
+    return fromDegrees(arcMinutes / 60.0);
+  }
+  
+  /**
+    * Constructs an angle from @p arcSeconds. An arc second is
+    * \f$\frac{1}{3600}\f$ of a degree.
+    */
+  inline static Angle<T> fromArcSeconds(const T& arcSeconds) {
+    return fromArcMinutes(arcSeconds / 60.0);
+  }
+  
+  /**
+    * Constructs an angle from @p hexacontades. A hexacontade is
+    * \f$\frac{1}{60}\f$ of a turn.
+    */
+  inline static Angle<T> fromHexacontades(const T& hexacontades) {
+    return fromTurns(hexacontades / 60.0);
+  }
+  
+  /**
+    * Constructs an angle from @p binDegrees. A binary degree is
+    * \f$\frac{1}{256}\f$ of a turn. Although the resolution is a bit lower,
+    * this allows angles to be stored in 8 bits.
+    */
+  inline static Angle<T> fromBinaryDegrees(const T& binDegrees) {
+    return fromTurns(binDegrees / 256.0);
+  }
+  
+  /**
     * Constructs an angle from @p radians. The following interactive figure
     * shows the angle in radians. Click and drag horizontally to change the
     * angle.
@@ -65,6 +108,14 @@ public:
     */
   inline static Angle<T> fromRadians(const T& radians) {
     return Angle<T>(radians);
+  }
+  
+  /**
+    * Constructs an angle from @p grads. A grad is \f$\frac{1}{400}\f$ of a
+    * turn.
+    */
+  inline static Angle<T> fromGrads(const T& grads) {
+    return fromTurns(grads / 400.0);
   }
   
   /**
@@ -85,6 +136,22 @@ public:
   }
   
   /**
+    * Constructs an angle from @p quadrants. A quadrant is \f$\frac{1}{4}\f$ of
+    * a turn.
+    */
+  inline static Angle<T> fromQuadrants(const T& quadrants) {
+    return fromTurns(quadrants / 4.0);
+  }
+  
+  /**
+    * Constructs an angle from @p sextants. A sextant is \f$\frac{1}{4}\f$ of a
+    * turn.
+    */
+  inline static Angle<T> fromSextants(const T& sextants) {
+    return fromTurns(sextants / 6.0);
+  }
+  
+  /**
     * Constructs an angle from @p oClock. Internally, the angle is stored in
     * radians, with \f$r = \frac{o \times 2\pi}{12}\f$, where \f$o\f$ is
     * @p oClock (the hour on a clock) and \f$r\f$ is the angle in radians. The
@@ -102,6 +169,22 @@ public:
   }
   
   /**
+    * Constructs an angle from @p hours. An hour is \f$\frac{1}{24}\f$ of a
+    * turn.
+    */
+  inline static Angle<T> fromHours(const T& hours) {
+    return fromTurns(hours / 24.0);
+  }
+  
+  /**
+    * Constructs an angle from @p compassPoints. A compass point is
+    * \f$\frac{1}{32}\f$ of a turn.
+    */
+  inline static Angle<T> fromCompassPoints(const T& compassPoints) {
+    return fromTurns(compassPoints / 32.0);
+  }
+  
+  /**
     * @returns the angle in degrees, \f$d = r \times \frac{360}{2\pi}\f$, where
     *   \f$d\f$ is the angle in degrees and \f$r\f$ is the angle in radians.
     */
@@ -110,10 +193,45 @@ public:
   }
   
   /**
+    * @returns the angle in arc minutes, which is degrees() times 60.
+    */
+  inline T arcMinutes() const {
+    return degrees() * 60.0;
+  }
+  
+  /**
+    * @returns the angle in arc seconds, which is degrees() times 3600.
+    */
+  inline T arcSeconds() const {
+    return arcMinutes() * 60.0;
+  }
+  
+  /**
+    * @returns the angle in hexacontades, which is turns() times 60.
+    */
+  inline T hexacontades() const {
+    return turns() * 60.0;
+  }
+  
+  /**
+    * @returns the angle in binary degrees, which is turns() times 256.
+    */
+  inline T binaryDegrees() const {
+    return turns() * 256.0;
+  }
+  
+  /**
     * @returns the angle in radians.
     */
   inline T radians() const {
     return m_radians;
+  }
+  
+  /**
+    * @returns the angle in grads, which is turns() times 400.
+    */
+  inline T grads() const {
+    return turns() * 400.0;
   }
   
   /**
@@ -125,12 +243,40 @@ public:
   }
   
   /**
+    * @returns the angle in quadrants, which is turns() times 4.
+    */
+  inline T quadrants() const {
+    return turns() * 4.0;
+  }
+  
+  /**
+    * @returns the angle in sextants, which is turns() times 6.
+    */
+  inline T sextants() const {
+    return turns() * 6.0;
+  }
+  
+  /**
     * @returns the angle in hours on the clock, \f$o = \frac{12r}{2\pi}\f$,
     *   where \f$o\f$ is the angle in hours on the clock and \f$r\f$ is the
     *   angle in radians.
     */
   inline T oclock() const {
     return m_radians * 1.909859317102744029227;
+  }
+  
+  /**
+    * @returns the angle in hours, which is turns() times 24.
+    */
+  inline T hours() const {
+    return turns() * 24.0;
+  }
+  
+  /**
+    * @returns the angle in compass points, which is turns() times 32.
+    */
+  inline T compassPoints() const {
+    return turns() * 32.0;
   }
   
   /**
@@ -223,6 +369,46 @@ typedef Angle<double> Angled;
 const Anglef Degreef = Anglef::fromDegrees(1);
 
 /**
+  * Constant float angle of one arc minute. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromArcMinutes().
+  * 
+  * @code
+  * Anglef angle = 5400 * ArcMinutef;
+  * @endcode
+  */
+const Anglef ArcMinutef = Anglef::fromArcMinutes(1);
+
+/**
+  * Constant float angle of one arc second. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromArcSeconds().
+  * 
+  * @code
+  * Anglef angle = 324000 * ArcSecondf;
+  * @endcode
+  */
+const Anglef ArcSecondf = Anglef::fromArcSeconds(1);
+
+/**
+  * Constant float angle of one hexacontade. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromHexacontades().
+  * 
+  * @code
+  * Anglef angle = 15 * Hexacontadef;
+  * @endcode
+  */
+const Anglef Hexacontadef = Anglef::fromHexacontades(1);
+
+/**
+  * Constant float angle of one binary degree. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromBinaryDegrees().
+  * 
+  * @code
+  * Anglef angle = 64 * BinaryDegreef;
+  * @endcode
+  */
+const Anglef BinaryDegreef = Anglef::fromBinaryDegrees(1);
+
+/**
   * Constant float angle of one radian. Use it with the multiplication operator
   * as a shortcut to Anglef::fromRadians().
   * 
@@ -231,6 +417,16 @@ const Anglef Degreef = Anglef::fromDegrees(1);
   * @endcode
   */
 const Anglef Radianf = Anglef::fromRadians(1);
+
+ /**
+  * Constant float angle of one grad. Use it with the multiplication operator
+  * as a shortcut to Anglef::fromGrads().
+  * 
+  * @code
+  * Anglef angle = 100 * Gradf;
+  * @endcode
+  */
+const Anglef Gradf = Anglef::fromGrads(1);
 
 /**
   * Constant float angle of one turn. Use it with the multiplication operator
@@ -243,6 +439,26 @@ const Anglef Radianf = Anglef::fromRadians(1);
 const Anglef Turnf = Anglef::fromTurns(1);
 
 /**
+  * Constant float angle of one quadrant. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromQuadrants().
+  * 
+  * @code
+  * Anglef angle = 1 * Quadrantf;
+  * @endcode
+  */
+const Anglef Quadrantf = Anglef::fromQuadrants(1);
+
+/**
+  * Constant float angle of one sextant. Use it with the multiplication operator
+  * as a shortcut to Anglef::fromSextants().
+  * 
+  * @code
+  * Anglef angle = 2 * Sextantf;
+  * @endcode
+  */
+const Anglef Sextantf = Anglef::fromSextants(1);
+
+/**
   * Constant float angle of one hour. Use it with the multiplication operator
   * as a shortcut to Anglef::fromClock().
   * 
@@ -251,6 +467,26 @@ const Anglef Turnf = Anglef::fromTurns(1);
   * @endcode
   */
 const Anglef oClockf = Anglef::fromClock(1);
+
+/**
+  * Constant float angle of one hour. Use it with the multiplication operator
+  * as a shortcut to Anglef::fromHours().
+  * 
+  * @code
+  * Anglef angle = 6 * Hourf;
+  * @endcode
+  */
+const Anglef Hourf = Anglef::fromHours(1);
+
+/**
+  * Constant float angle of one compass point. Use it with the multiplication
+  * operator as a shortcut to Anglef::fromCompassPoints().
+  * 
+  * @code
+  * Anglef angle = 90 * CompassPointf;
+  * @endcode
+  */
+const Anglef CompassPointf = Anglef::fromCompassPoints(1);
 
 /**
   * Constant double angle of one degree. Use it with the multiplication operator
@@ -263,14 +499,64 @@ const Anglef oClockf = Anglef::fromClock(1);
 const Angled Degreed = Angled::fromDegrees(1);
 
 /**
+  * Constant double angle of one arc minute. Use it with the multiplication
+  * operator as a shortcut to Angled::fromArcMinutes().
+  * 
+  * @code
+  * Angled angle = 90 * ArcMinuted;
+  * @endcode
+  */
+const Angled ArcMinuted = Angled::fromArcMinutes(1);
+
+/**
+  * Constant double angle of one arc second. Use it with the multiplication
+  * operator as a shortcut to Angled::fromArcSeconds().
+  * 
+  * @code
+  * Angled angle = 5400 * ArcSecondd;
+  * @endcode
+  */
+const Angled ArcSecondd = Angled::fromArcSeconds(1);
+
+/**
+  * Constant double angle of one hexacontade. Use it with the multiplication
+  * operator as a shortcut to Angled::fromHexacontades().
+  * 
+  * @code
+  * Angled angle = 324000 * Hexacontaded;
+  * @endcode
+  */
+const Angled Hexacontaded = Angled::fromHexacontades(1);
+
+/**
+  * Constant double angle of one binary degree. Use it with the multiplication
+  * operator as a shortcut to Angled::fromBinaryDegrees().
+  * 
+  * @code
+  * Angled angle = 64 * Degreed;
+  * @endcode
+  */
+const Angled BinaryDegreed = Angled::fromBinaryDegrees(1);
+
+/**
   * Constant double angle of one radian. Use it with the multiplication operator
   * as a shortcut to Angled::fromRadians().
   * 
   * @code
-  * Angled angle = 2 * Radiand;
+  * Angled angle = 2 * Angled;
   * @endcode
   */
 const Angled Radiand = Angled::fromRadians(1);
+
+/**
+  * Constant double angle of one grad. Use it with the multiplication operator
+  * as a shortcut to Angled::fromGrads().
+  * 
+  * @code
+  * Angled angle = 90 * Gradd;
+  * @endcode
+  */
+const Angled Gradd = Angled::fromGrads(1);
 
 /**
   * Constant double angle of one turn. Use it with the multiplication operator
@@ -283,6 +569,26 @@ const Angled Radiand = Angled::fromRadians(1);
 const Angled Turnd = Angled::fromTurns(1);
 
 /**
+  * Constant double angle of one quadrant. Use it with the multiplication
+  * operatoras a shortcut to Angled::fromQuadrants().
+  * 
+  * @code
+  * Angled angle = 1 * Quadrantd;
+  * @endcode
+  */
+const Angled Quadrantd = Angled::fromQuadrants(1);
+
+/**
+  * Constant double angle of one sextant. Use it with the multiplication
+  * operator as a shortcut to Angled::fromSextants().
+  * 
+  * @code
+  * Angled angle = 90 * Sextantd;
+  * @endcode
+  */
+const Angled Sextantd = Angled::fromSextants(1);
+
+/**
   * Constant double angle of one hour. Use it with the multiplication operator
   * as a shortcut to Angled::fromClock().
   * 
@@ -293,6 +599,26 @@ const Angled Turnd = Angled::fromTurns(1);
 const Angled oClockd = Angled::fromClock(1);
 
 /**
+  * Constant double angle of one hour. Use it with the multiplication operator
+  * as a shortcut to Angled::fromHours().
+  * 
+  * @code
+  * Angled angle = 6 * Hourd;
+  * @endcode
+  */
+const Angled Hourd = Angled::fromHours(1);
+
+/**
+  * Constant double angle of one compass point. Use it with the multiplication
+  * operator as a shortcut to Angled::fromCompassPoints().
+  * 
+  * @code
+  * Angled angle = 8 * Degreed;
+  * @endcode
+  */
+const Angled CompassPointd = Angled::fromCompassPoints(1);
+
+/**
   * Suffix operator for floating point literals.
   * 
   * @returns an Angle<double> with the degrees from the literal.
@@ -301,6 +627,50 @@ const Angled oClockd = Angled::fromClock(1);
   */
 inline Angled operator "" _degrees(long double value) {
   return Angled::fromDegrees(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @returns an Angle<double> with the arc minutes from the literal.
+  * 
+  * @see Angle<double>::fromArcMinutes().
+  */
+inline Angled operator "" _arc_minutes(long double value) {
+  return Angled::fromArcMinutes(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @returns an Angle<double> with the arc seconds from the literal.
+  * 
+  * @see Angle<double>::fromArcSeconds().
+  */
+inline Angled operator "" _arc_seconds(long double value) {
+  return Angled::fromArcSeconds(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @returns an Angle<double> with the hexacontades from the literal.
+  * 
+  * @see Angle<double>::fromHexacontades().
+  */
+inline Angled operator "" _hexacontades(long double value) {
+  return Angled::fromHexacontades(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @returns an Angle<double> with the binary degrees from the literal.
+  * 
+  * @see Angle<double>::fromBinaryDegrees().
+  */
+inline Angled operator "" _binary_degrees(long double value) {
+  return Angled::fromBinaryDegrees(value);
 }
 
 /**
@@ -315,6 +685,50 @@ inline Angled operator "" _degrees(unsigned long long int value) {
 }
 
 /**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromArcMinutes().
+  * 
+  * @returns an Angle<double> with the arc minutes from the literal.
+  */
+inline Angled operator "" _arc_minutes(unsigned long long int value) {
+  return Angled::fromArcMinutes(value);
+}
+
+/**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromArcSeconds().
+  * 
+  * @returns an Angle<double> with the arc seconds from the literal.
+  */
+inline Angled operator "" _arc_seconds(unsigned long long int value) {
+  return Angled::fromArcSeconds(value);
+}
+
+/**
+  * Suffix operator for hexacontades.
+  * 
+  * @see Angle<double>::fromDegrees().
+  * 
+  * @returns an Angle<double> with the degrees from the literal.
+  */
+inline Angled operator "" _hexacontades(unsigned long long int value) {
+  return Angled::fromHexacontades(value);
+}
+
+/**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromBinaryDegrees().
+  * 
+  * @returns an Angle<double> with the degrees from the literal.
+  */
+inline Angled operator "" _binary_degrees(unsigned long long int value) {
+  return Angled::fromBinaryDegrees(value);
+}
+
+/**
   * Suffix operator for floating point literals.
   * 
   * @see Angle<double>::fromRadians().
@@ -323,6 +737,17 @@ inline Angled operator "" _degrees(unsigned long long int value) {
   */
 inline Angled operator "" _radians(long double value) {
   return Angled::fromRadians(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @see Angle<double>::fromGrad().
+  * 
+  * @returns an Angle<double> with the grads from the literal.
+  */
+inline Angled operator "" _grads(long double value) {
+  return Angled::fromGrads(value);
 }
 
 /**
@@ -337,6 +762,17 @@ inline Angled operator "" _radians(unsigned long long int value) {
 }
 
 /**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromGrads().
+  * 
+  * @returns an Angle<double> with the grads from the literal.
+  */
+inline Angled operator "" _grads(unsigned long long int value) {
+  return Angled::fromGrads(value);
+}
+
+/**
   * Suffix operator for floating point literals.
   * 
   * @see Angle<double>::fromTurns().
@@ -345,6 +781,28 @@ inline Angled operator "" _radians(unsigned long long int value) {
   */
 inline Angled operator "" _turns(long double value) {
   return Angled::fromTurns(value);
+}
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @see Angle<double>::fromQuadrants().
+  * 
+  * @returns an Angle<double> with the quadrants from the literal.
+  */
+
+inline Angled operator "" _quadrants(long double value) {
+  return Angled::fromQuadrants(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @see Angle<double>::fromSextants().
+  * 
+  * @returns an Angle<double> with the turns from the literal.
+  */
+inline Angled operator "" _sextants(long double value) {
+  return Angled::fromSextants(value);
 }
 
 /**
@@ -359,13 +817,68 @@ inline Angled operator "" _turns(unsigned long long int value) {
 }
 
 /**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromQuadrants().
+  * 
+  * @returns an Angle<double> with the quadrants from the literal.
+  */
+inline Angled operator "" _quadrants(unsigned long long int value) {
+  return Angled::fromQuadrants(value);
+}
+
+/**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromSextants().
+  * 
+  * @returns an Angle<double> with the sextants from the literal.
+  */
+inline Angled operator "" _sextants(unsigned long long int value) {
+  return Angled::fromSextants(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @see Angle<double>::fromClock().
+  * 
+  * @returns an Angle<double> with the hours from the literal.
+  */
+inline Angled operator "" _oclock(long double value) {
+  return Angled::fromClock(value);
+}
+
+/**
   * Suffix operator for floating point literals.
   * 
   * @see Angle<double>::fromHours().
   * 
-  * @returns an Angle<double> with the hours on the clock from the literal.
+  * @returns an Angle<double> with the hours literal.
   */
-inline Angled operator "" _oclock(long double value) {
+inline Angled operator "" _hours(long double value) {
+  return Angled::fromHours(value);
+}
+
+/**
+  * Suffix operator for floating point literals.
+  * 
+  * @see Angle<double>::fromCompassPoints().
+  * 
+  * @returns an Angle<double> with the compass points from the literal.
+  */
+inline Angled operator "" _compass_points(long double value) {
+  return Angled::fromCompassPoints(value);
+}
+
+/**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromHours().
+  * 
+  * @returns an Angle<double> with the hours from the literal.
+  */
+inline Angled operator "" _oclock(unsigned long long int value) {
   return Angled::fromClock(value);
 }
 
@@ -374,8 +887,19 @@ inline Angled operator "" _oclock(long double value) {
   * 
   * @see Angle<double>::fromHours().
   * 
-  * @returns an Angle<double> with the hours on the clock from the literal.
+  * @returns an Angle<double> with the hours from the literal.
   */
-inline Angled operator "" _oclock(unsigned long long int value) {
-  return Angled::fromClock(value);
+inline Angled operator "" _hours(unsigned long long int value) {
+  return Angled::fromHours(value);
+}
+
+/**
+  * Suffix operator for integer literals.
+  * 
+  * @see Angle<double>::fromCompassPoints().
+  * 
+  * @returns an Angle<double> with the compass points from the literal.
+  */
+inline Angled operator "" _compass_points(unsigned long long int value) {
+  return Angled::fromCompassPoints(value);
 }
--- test/unit/core/AngleTest.cpp
+++ test/unit/core/AngleTest.cpp
@@ -25,9 +25,34 @@ namespace AngleTest {
     ASSERT_NEAR(90, copy.degrees(), 0.0001);
   }
   
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromArcMinutes) {
+    auto angle = Angle<TypeParam>::fromArcMinutes(5400);
+    ASSERT_NEAR(5400, angle.arcMinutes(), 0.0001);
+  }
+  
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromArcSeconds) {
+    auto angle = Angle<TypeParam>::fromArcSeconds(324000);
+    ASSERT_NEAR(324000, angle.arcSeconds(), 0.0001);
+  }
+  
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromHexacontades) {
+    auto angle = Angle<TypeParam>::fromHexacontades(15);
+    ASSERT_NEAR(15, angle.hexacontades(), 0.0001);
+  }
+  
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromBinaryDegrees) {
+    auto angle = Angle<TypeParam>::fromBinaryDegrees(64);
+    ASSERT_NEAR(64, angle.binaryDegrees(), 0.0001);
+  }
+  
   TYPED_TEST(AngleTest, ShouldCreateAngleFromRadians) {
     auto angle = Angle<TypeParam>::fromRadians(5);
     ASSERT_NEAR(5, angle.radians(), 0.0001);
+  }
+
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromGrads) {
+    auto angle = Angle<TypeParam>::fromGrads(100);
+    ASSERT_NEAR(100, angle.grads(), 0.0001);
   }
 
   TYPED_TEST(AngleTest, ShouldCreateAngleFromDegrees) {
@@ -40,9 +65,29 @@ namespace AngleTest {
     ASSERT_NEAR(8, angle.turns(), 0.0001);
   }
 
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromQuadrants) {
+    auto angle = Angle<TypeParam>::fromQuadrants(1);
+    ASSERT_NEAR(1, angle.quadrants(), 0.0001);
+  }
+
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromSextants) {
+    auto angle = Angle<TypeParam>::fromSextants(2);
+    ASSERT_NEAR(2, angle.sextants(), 0.0001);
+  }
+
   TYPED_TEST(AngleTest, ShouldCreateAngleFromClock) {
     auto angle = Angle<TypeParam>::fromClock(3);
     ASSERT_NEAR(3, angle.oclock(), 0.0001);
+  }
+  
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromHours) {
+    auto angle = Angle<TypeParam>::fromHours(6);
+    ASSERT_NEAR(6, angle.hours(), 0.0001);
+  }
+  
+  TYPED_TEST(AngleTest, ShouldCreateAngleFromCompassPoints) {
+    auto angle = Angle<TypeParam>::fromCompassPoints(16);
+    ASSERT_NEAR(16, angle.compassPoints(), 0.0001);
   }
   
   TYPED_TEST(AngleTest, ShouldConvertBetweenUnits) {
@@ -105,12 +150,52 @@ namespace AngleTest {
     ASSERT_NEAR(5, angled.degrees(), 0.0001);
   }
   
+  TEST(AngleTest, ShouldHaveArcMinuteShortCuts) {
+    auto anglef = 5 * ArcMinutef;
+    ASSERT_NEAR(5, anglef.arcMinutes(), 0.0001);
+
+    auto angled = 5 * ArcMinuted;
+    ASSERT_NEAR(5, angled.arcMinutes(), 0.0001);
+  }
+
+  TEST(AngleTest, ShouldHaveArcSecondShortCuts) {
+    auto anglef = 5 * ArcSecondf;
+    ASSERT_NEAR(5, anglef.arcSeconds(), 0.0001);
+
+    auto angled = 5 * ArcSecondd;
+    ASSERT_NEAR(5, angled.arcSeconds(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveHexacontadeShortCuts) {
+    auto anglef = 5 * Hexacontadef;
+    ASSERT_NEAR(5, anglef.hexacontades(), 0.0001);
+
+    auto angled = 5 * Hexacontaded;
+    ASSERT_NEAR(5, angled.hexacontades(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveBinaryDegreeShortCuts) {
+    auto anglef = 5 * BinaryDegreef;
+    ASSERT_NEAR(5, anglef.binaryDegrees(), 0.0001);
+
+    auto angled = 5 * BinaryDegreed;
+    ASSERT_NEAR(5, angled.binaryDegrees(), 0.0001);
+  }
+  
   TEST(AngleTest, ShouldHaveRadianShortCuts) {
     auto anglef = 5 * Radianf;
     ASSERT_NEAR(5, anglef.radians(), 0.0001);
 
     auto angled = 5 * Radiand;
     ASSERT_NEAR(5, angled.radians(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveGradShortCuts) {
+    auto anglef = 5 * Gradf;
+    ASSERT_NEAR(5, anglef.grads(), 0.0001);
+
+    auto angled = 5 * Gradd;
+    ASSERT_NEAR(5, angled.grads(), 0.0001);
   }
   
   TEST(AngleTest, ShouldHaveTurnShortCuts) {
@@ -121,12 +206,36 @@ namespace AngleTest {
     ASSERT_NEAR(5, angled.turns(), 0.0001);
   }
   
+  TEST(AngleTest, ShouldHaveQuadrantShortCuts) {
+    auto anglef = 5 * Quadrantf;
+    ASSERT_NEAR(5, anglef.quadrants(), 0.0001);
+
+    auto angled = 5 * Quadrantd;
+    ASSERT_NEAR(5, angled.quadrants(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveSextantShortCuts) {
+    auto anglef = 5 * Sextantf;
+    ASSERT_NEAR(5, anglef.sextants(), 0.0001);
+
+    auto angled = 5 * Sextantd;
+    ASSERT_NEAR(5, angled.sextants(), 0.0001);
+  }
+  
   TEST(AngleTest, ShouldHaveClockShortCuts) {
     auto anglef = 5 * oClockf;
     ASSERT_NEAR(5, anglef.oclock(), 0.0001);
 
     auto angled = 5 * oClockd;
     ASSERT_NEAR(5, angled.oclock(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveHourShortCuts) {
+    auto anglef = 5 * Hourf;
+    ASSERT_NEAR(5, anglef.hours(), 0.0001);
+
+    auto angled = 5 * Hourd;
+    ASSERT_NEAR(5, angled.hours(), 0.0001);
   }
   
   TEST(AngleTest, ShouldHaveDegreesSuffixOperator) {
@@ -137,12 +246,36 @@ namespace AngleTest {
     ASSERT_NEAR(7.1, angled.degrees(), 0.0001);
   }
   
+  TEST(AngleTest, ShouldHaveArcMinutesSuffixOperator) {
+    auto anglei = 5_arc_minutes;
+    ASSERT_NEAR(5, anglei.arcMinutes(), 0.0001);
+
+    auto angled = 7.1_arc_minutes;
+    ASSERT_NEAR(7.1, angled.arcMinutes(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveArcSecondsSuffixOperator) {
+    auto anglei = 5_arc_seconds;
+    ASSERT_NEAR(5, anglei.arcSeconds(), 0.0001);
+
+    auto angled = 7.1_arc_seconds;
+    ASSERT_NEAR(7.1, angled.arcSeconds(), 0.0001);
+  }
+  
   TEST(AngleTest, ShouldHaveRadiansSuffixOperator) {
     auto anglei = 5_radians;
     ASSERT_NEAR(5, anglei.radians(), 0.0001);
 
     auto angled = 7.1_radians;
     ASSERT_NEAR(7.1, angled.radians(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveGradsSuffixOperator) {
+    auto anglei = 5_grads;
+    ASSERT_NEAR(5, anglei.grads(), 0.0001);
+
+    auto angled = 7.1_grads;
+    ASSERT_NEAR(7.1, angled.grads(), 0.0001);
   }
   
   TEST(AngleTest, ShouldHaveTurnsSuffixOperator) {
@@ -153,11 +286,35 @@ namespace AngleTest {
     ASSERT_NEAR(7.1, angled.turns(), 0.0001);
   }
   
+  TEST(AngleTest, ShouldHaveQuadrantsSuffixOperator) {
+    auto anglei = 5_quadrants;
+    ASSERT_NEAR(5, anglei.quadrants(), 0.0001);
+
+    auto angled = 7.1_quadrants;
+    ASSERT_NEAR(7.1, angled.quadrants(), 0.0001);
+  }
+  
+  TEST(AngleTest, ShouldHaveSextantsSuffixOperator) {
+    auto anglei = 5_sextants;
+    ASSERT_NEAR(5, anglei.sextants(), 0.0001);
+
+    auto angled = 7.1_sextants;
+    ASSERT_NEAR(7.1, angled.sextants(), 0.0001);
+  }
+  
   TEST(AngleTest, ShouldHaveOClockSuffixOperator) {
     auto anglei = 5_oclock;
     ASSERT_NEAR(5, anglei.oclock(), 0.0001);
 
     auto angled = 7.1_oclock;
     ASSERT_NEAR(7.1, angled.oclock(), 0.0001);
+  }
+
+  TEST(AngleTest, ShouldHaveHourSuffixOperator) {
+    auto anglei = 5_hours;
+    ASSERT_NEAR(5, anglei.hours(), 0.0001);
+
+    auto angled = 7.1_hours;
+    ASSERT_NEAR(7.1, angled.hours(), 0.0001);
   }
 }
